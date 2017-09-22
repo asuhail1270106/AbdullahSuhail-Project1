e1bd3b3443deddb6a6541bf00ffdb014ef57f4d9
asuhail1270106 <asuhail@hawk.iit.edu>
2017-09-21 21:27:54 -0500

    fixing indents

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function subscribed()
 {
-	alert("Your life is about to get CRAZY better! Not really, but you'll know about Assassin's Creed stuff.");
-	return true;
+  alert("Your life is about to get CRAZY better! Not really, but you'll know about Assassin's Creed stuff.");
+  return true;
 }
